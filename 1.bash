6c2a21c20d68f888ac58de60c0f6dd66712bb5f9
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-05-22 09:18:00 -0700

    Create 1.bash

---
+++ 1.bash
@@ -0,0 +1,11 @@
+apt update
+apt install nginx-full
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install nodejs
+mkdir /opt/VpnAdmin
+cd /opt/VpnAdmin
+npm install node-telegram-bot-api
+npm install express
+npm install redis
+npm install nodemiral
+apt install redis-cli
